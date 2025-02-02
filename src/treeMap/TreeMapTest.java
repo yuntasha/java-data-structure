e0e2fac2a0db0e8f416001002a14462237933321
yuntasha <jaljayo85@gmail.com>
2025-02-02 17:42:43 +0900

    feat: TreeMap과 TreeSet 실험 - 비교 값이 같으면 중복으로 처리됩니다.

---
+++ src/treeMap/TreeMapTest.java
@@ -0,0 +1,79 @@
+package treeMap;
+
+import java.util.*;
+
+public class TreeMapTest {
+    public static void main(String[] args) {
+        testSet();
+        testMap();
+    }
+
+    static void testSet() {
+//        Set<Node> set = new HashSet<>();
+//        Set<Node> set = new TreeSet<>(Comparator.comparingInt(Node::getValue));
+        Set<Node> set = new TreeSet<>(Comparator.comparingInt(Node::getValue)
+                .thenComparingInt(Node::getNum));
+
+        set.add(new Node(1, 3));
+        System.out.println(set.size());
+
+        set.add(new Node(2, 4));
+        System.out.println(set.size());
+
+        set.add(new Node(3, 4));
+        System.out.println(set.size());
+
+        for (Node node : set) {
+            System.out.println(node.num + " " + node.value);
+        }
+    }
+
+    static void testMap() {
+//        Map<Node, Integer> map = new HashMap<>();
+//        Map<Node, Integer> map = new TreeMap<>(Comparator.comparingInt(Node::getValue));
+        Map<Node, Integer> map = new TreeMap<>(Comparator.comparingInt(Node::getValue)
+                .thenComparingInt(Node::getNum));
+
+        map.put(new Node(1, 3), 1);
+        System.out.println(map.size());
+
+        map.put(new Node(2, 4), 2);
+        System.out.println(map.size());
+
+        map.put(new Node(3, 4), 3);
+        System.out.println(map.size());
+
+        for (Map.Entry<Node, Integer> en : map.entrySet()) {
+            System.out.println(en.getKey().num + " " + en.getValue());
+        }
+    }
+
+    static class Node {
+        int num;
+        int value;
+
+        public Node(int num, int value) {
+            this.num = num;
+            this.value = value;
+        }
+
+        public int getNum() {
+            return num;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+//        public int getValue() {
+//            return value * 1000 + num;
+//        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof Node)) return false;
+            Node n = (Node) obj;
+            return this.num == n.num && this.value == n.value;
+        }
+    }
+}
